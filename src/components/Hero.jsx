b550f0828d7bd8a9de2e0e24e9dc0ef6ac8ab248
nihal <nihalmmuneer@gmail.com>
2024-12-12 13:12:10 +0530

    resolved swipe issue

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,12 @@
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
-import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
+    <section className="relative w-full h-screen mx-auto">
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
@@ -25,7 +24,8 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+      {/* Ensure canvas doesn't block touch events */}
+      <ComputersCanvas className="w-full h-full pointer-events-none" />
 
       <div className="relative xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
